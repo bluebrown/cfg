a70c7e5feabdbd60fa8b642ace05bd9436eb3f74
Nico Braun <rainbowstack@gmail.com>
2024-11-11 10:55:27 +0100

    improve sarplot
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- assets/sarplot.sh
+++ assets/sarplot.sh
@@ -12,17 +12,17 @@ sar -f "$file" -n SOCK | sed '1,2d;$d' >/tmp/sock.dat
 
 cat <<EOF | gnuplot >"stats.png"
 set terminal pngcairo dashed size 1600,900 background rgb "#0d1117"
-set label font "Arial,12" textcolor rgb "#9198a1"
-set xtics textcolor rgb "#9198a1"
-set ytics textcolor rgb "#9198a1"
-set style line 10 linecolor rgb "#6a6e7487" dashtype 3
+set label font "Arial,12" textcolor rgb "#f0f6fc"
+set xtics textcolor rgb "#f0f6fc"
+set ytics textcolor rgb "#f0f6fc"
+set style line 10 linecolor rgb "#a3a3a3" dashtype 3
 set style line 1 lt 1 lc rgb "#4493f8"
 set style line 2 lt 1 lc rgb "#3fb950"
 
 set grid linestyle 10
 set style data lines
 
-set key autotitle columnhead outside textcolor rgb "#9198a1"
+set key autotitle columnhead outside textcolor rgb "#f0f6fc"
 
 set xdata time
 set timefmt "%H:%M:%S"
@@ -30,18 +30,18 @@ set format x "%H:%M:%S"
 set yrange [0:*]
 set autoscale
 
-set multiplot layout 4,1 title "System Statistics" textcolor rgb "#9198a1" font "Arial,16"
+set multiplot layout 4,1 title "System Statistics" textcolor rgb "#f0f6fc" font "Arial,16"
 
-set title "CPU" textcolor rgb "#9198a1"
-plot "/tmp/cpu.dat" using 1:(100 - column(8)) ls 1 smooth freq title "%busy"
+set title "CPU" textcolor rgb "#f0f6fc"
+plot "/tmp/cpu.dat" using 1:(100 - column(8)) ls 1 smooth freq title "CPU Busy (%)"
 
-set title "Memory" textcolor rgb "#9198a1"
-plot "/tmp/mem.dat" using 1:5 ls 1 smooth freq
+set title "Memory" textcolor rgb "#f0f6fc"
+plot "/tmp/mem.dat" using 1:(column(4) / 1048576) ls 1 smooth freq title "Memory Used (GB)"
 
-set title "Net I/O" textcolor rgb "#9198a1"
-plot "/tmp/dev.dat" using 1:5 ls 1 smooth freq, \
-     "/tmp/dev.dat" using 1:6 ls 2 smooth freq
+set title "Net I/O" textcolor rgb "#f0f6fc"
+plot "/tmp/dev.dat" using 1:(column(5) * 8 / 1000) ls 1 smooth freq title "TX (mB/s)", \
+     "/tmp/dev.dat" using 1:(column(6) * 8 / 1000) ls 2 smooth freq title "RX (mB/s)"
 
-set title "Sockets" textcolor rgb "#9198a1"
-plot "/tmp/sock.dat" using 1:2 ls 1 smooth freq
+set title "Sockets" textcolor rgb "#f0f6fc"
+plot "/tmp/sock.dat" using 1:2 ls 1 smooth freq title "Open Sockets"
 EOF
